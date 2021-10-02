9cc05368d6b653d293ba609289e345859b23afa6
Sanjay <59828852+sanjay-swain@users.noreply.github.com>
2021-10-03 00:11:59 +0530

    Created url path to chapters page

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import subject_view
+from .views import subject_view, chapter_view
 
 urlpatterns = [
-    path('', subject_view, name="subjects")
+    path('', subject_view, name="subjects"),
+    path('<str:subject>/', chapter_view, name="chapters")
 ]
