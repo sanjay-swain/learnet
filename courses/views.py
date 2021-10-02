07427b71f133550d853a028b1896668151b58918
Sanjay <59828852+sanjay-swain@users.noreply.github.com>
2021-10-02 14:33:03 +0530

    Created subject view
    Created subject view for courses app

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Class, Subject, Chapter, Topic, Video
 
-# Create your views here.
+
+def subject_view(request):
+    subject_list = {}
+    for subject in Subject.objects.all():
+        if len(Chapter.objects.filter(subject_id=subject)) > 1:
+            subject_list[subject] = [Chapter.objects.filter(subject_id=subject)[:3]] + \
+                                    [Chapter.objects.filter(subject_id=subject)[3:6]]
+        else:
+            subject_list[subject] = []
+    context = {
+        'subjects': subject_list
+    }
+    template_view = 'courses/subjects_page.html'
+    return render(request, template_view, context)
