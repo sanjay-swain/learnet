fbe3ef81ac909d655868e5fd69e79dc1bf70c669
Sanjay <59828852+sanjay-swain@users.noreply.github.com>
2021-10-02 14:02:52 +0530

    Update urls.py
    Add courses app to project urls

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('pages.urls'))
+    path('courses/', include('courses.urls'))
 ]
