9a5f5a914785ae748b3d0dbe2da149a190e071c4
Sanjay <59828852+sanjay-swain@users.noreply.github.com>
2021-10-03 14:38:44 +0530

    Added video page urls to application

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import chapter_detail_view, subject_view, chapter_view
+from .views import chapter_detail_view, subject_view, chapter_view, video_detail_view
 
 urlpatterns = [
     path('', subject_view, name="subjects"),
     path('<str:subject>/', chapter_view, name="chapters"),
-    path('<str:subject>/<str:chapter>/', chapter_detail_view, name="chapter")
+    path('<str:subject>/<str:chapter>/', chapter_detail_view, name="chapter"),
+    path('<str:subject>/<str:chapter>/<str:video>/', video_detail_view, name="video")
 ]
