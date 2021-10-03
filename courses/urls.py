ea9ca641f35f40bda60ddb94ba69fcd674dffee9
Sanjay <59828852+sanjay-swain@users.noreply.github.com>
2021-10-03 09:17:27 +0530

    Update urls.py
    Added url to chapters page

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import subject_view, chapter_view
+from .views import chapter_detail_view, subject_view, chapter_view
 
 urlpatterns = [
     path('', subject_view, name="subjects"),
-    path('<str:subject>/', chapter_view, name="chapters")
+    path('<str:subject>/', chapter_view, name="chapters"),
+    path('<str:subject>/<str:chapter>/', chapter_detail_view, name="chapter")
 ]
