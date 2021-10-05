ccefdb360f7592a28838dc7f254331b22e8c97db
Sanjay <59828852+sanjay-swain@users.noreply.github.com>
2021-10-05 11:17:39 +0530

    Update login redirect

--- config/settings.py
+++ config/settings.py
@@ -120,5 +120,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 
 
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'subjects'
 LOGOUT_REDIRECT_URL = 'home'
