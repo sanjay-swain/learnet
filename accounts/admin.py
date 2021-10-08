cde6a18bf4a40a174cf873f0a012be6fe4db7854
Sanjay <59828852+sanjay-swain@users.noreply.github.com>
2021-10-08 10:46:18 +0530

    Modify user creation admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -20,6 +20,7 @@ class UserAdmin(DjangoUserAdmin):
             'classes': ('wide',),
             'fields': ('email', 'password1', 'password2'),
         }),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
     )
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
